6687da79b9a8c199c3f648c5a1a35dfe96cceca5
darekasan <darekasan.net@gmail.com>
2021-12-10 18:57:07 +0900

    add: サークルごとの購入作品数と販売終了作品がわかるように

--- dlst.js
+++ dlst.js
@@ -5,10 +5,11 @@ result.count = 0;
 result.totalPrice = 0;
 result.works = new Array();
 result.genreCount = new Map();
+result.makerCount = new Map();
+result.eol = new Array();
 var detailMode = true;
 
-for(var i=1;i<=lastPage;i++){
-    
+for(var i=1;i<=lastPage;i++){ // ページ走査    
     var doc = new DOMParser().parseFromString(fetchUrl(dlurl+i),"text/html");
     if(i==1){
         console.log(`取得中 ${i}ページ目`);
@@ -20,7 +21,7 @@ for(var i=1;i<=lastPage;i++){
         console.log(`取得中 ${i}/${lastPage}ページ目`);
     }
     var trElms = doc.querySelectorAll(".work_list_main tr:not(.item_name)");
-    trElms.forEach(elm => {
+    trElms.forEach(elm => { // 履歴の表の行走査
         var work = new Object();
         
         if(elm.querySelector(".work_name a")==null){
@@ -33,7 +34,6 @@ for(var i=1;i<=lastPage;i++){
         work.name = elm.querySelector(".work_name").innerText.trim();
         work.genre = elm.querySelector(".work_genre span").textContent.trim();
         work.price = parseInt(elm.querySelector(".work_price").innerText.replace(/\D/g, ''));
-
         work.makerName = elm.querySelector(".maker_name").innerText.trim();
 
         if(detailMode&&work.url!=""){
@@ -50,24 +50,46 @@ for(var i=1;i<=lastPage;i++){
             });
         }
 
+        if(!(result.makerCount.has(work.makerName))){
+            result.makerCount.set(work.makerName, 0);
+        }
+        result.makerCount.set(work.makerName, result.makerCount.get(work.makerName)+1);
+
         result.count++;
         result.totalPrice += work.price;
         result.works.push(work);
+        if(work.url==""){
+            result.eol.push(work);
+        }
     });
 }
 
 result.genreCount = new Map([...result.genreCount.entries()].sort(([, idA], [, idB]) =>  idB - idA));
+result.makerCount = new Map([...result.makerCount.entries()].sort(([, idA], [, idB]) =>  idB - idA));
 
-var ranking = "";
+var genreRanking = "";
 result.genreCount.forEach((value, key) =>{
-    ranking += key + ":" + value + " ";
+    genreRanking += key + ":" + value + " ";
+});
+
+var makerRanking = "";
+result.makerCount.forEach((value, key) =>{
+    makerRanking += key + ":" + value + " ";
 });
 
 console.log(JSON.stringify(result));
 console.log(`完了 作品数:${result.count} 合計金額:${result.totalPrice}`);
-console.log("ジャンル 多かった順\n"+ranking);
+console.log("ジャンル 多かった順\n"+genreRanking);
+console.log("サークル 多かった順\n"+makerRanking);
 
 
+if(result.eol.length>0){
+    var str=`販売終了作品数:${result.eol.length}\n`;
+    result.eol.forEach(work => {
+        str+=`${work.date} ${work.makerName} - ${work.name}\n`;
+    });
+    console.log(str);
+}
 
 function fetchUrl(url){
     var request = new XMLHttpRequest();
@@ -76,4 +98,3 @@ function fetchUrl(url){
     request.send(null);
     return request.responseText;
 }
-
